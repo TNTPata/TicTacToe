c739c952cd22301dacfc6fa0dc7da89069d2f884
TNTPata <putte.olsson@icloud.com>
2018-02-08 14:44:14 +0100

    Added player indicator labels. Added nine buttons with defined text characteristics.

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TicTacToe
 {
-	partial class Form1
+	partial class TicTacToe
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -28,12 +28,159 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.playerOneLabel = new System.Windows.Forms.Label();
+			this.playerTwoLabel = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// playerOneLabel
+			// 
+			this.playerOneLabel.AutoSize = true;
+			this.playerOneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.playerOneLabel.Location = new System.Drawing.Point(12, 9);
+			this.playerOneLabel.Name = "playerOneLabel";
+			this.playerOneLabel.Size = new System.Drawing.Size(102, 20);
+			this.playerOneLabel.TabIndex = 0;
+			this.playerOneLabel.Text = "Player One";
+			this.playerOneLabel.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// playerTwoLabel
+			// 
+			this.playerTwoLabel.AutoSize = true;
+			this.playerTwoLabel.Enabled = false;
+			this.playerTwoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.playerTwoLabel.Location = new System.Drawing.Point(151, 9);
+			this.playerTwoLabel.Name = "playerTwoLabel";
+			this.playerTwoLabel.Size = new System.Drawing.Size(102, 20);
+			this.playerTwoLabel.TabIndex = 1;
+			this.playerTwoLabel.Text = "Player Two";
+			this.playerTwoLabel.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(16, 50);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 75);
+			this.button1.TabIndex = 2;
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(97, 50);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 75);
+			this.button2.TabIndex = 3;
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.Location = new System.Drawing.Point(178, 50);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 75);
+			this.button3.TabIndex = 4;
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.Location = new System.Drawing.Point(16, 131);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 75);
+			this.button4.TabIndex = 5;
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button5.Location = new System.Drawing.Point(97, 131);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 75);
+			this.button5.TabIndex = 6;
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button6.Location = new System.Drawing.Point(178, 131);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(75, 75);
+			this.button6.TabIndex = 7;
+			this.button6.UseVisualStyleBackColor = true;
+			// 
+			// button7
+			// 
+			this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button7.Location = new System.Drawing.Point(16, 212);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(75, 75);
+			this.button7.TabIndex = 8;
+			this.button7.UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button8.Location = new System.Drawing.Point(97, 212);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(75, 75);
+			this.button8.TabIndex = 9;
+			this.button8.UseVisualStyleBackColor = true;
+			// 
+			// button9
+			// 
+			this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button9.Location = new System.Drawing.Point(178, 212);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(75, 75);
+			this.button9.TabIndex = 10;
+			this.button9.UseVisualStyleBackColor = true;
+			// 
+			// TicTacToe
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(271, 309);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.playerTwoLabel);
+			this.Controls.Add(this.playerOneLabel);
+			this.Name = "TicTacToe";
+			this.Text = "Tic Tac Toe";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label playerOneLabel;
+		private System.Windows.Forms.Label playerTwoLabel;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
 	}
 }
 
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace TicTacToe
 {
-	public partial class Form1 : Form
+	public partial class TicTacToe : Form
 	{
-		public Form1()
+		public TicTacToe()
 		{
 			InitializeComponent();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
