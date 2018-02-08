933f40be6bc0d4622e3b1b4da01f63fe1fc8bed2
TNTPata <putte.olsson@icloud.com>
2018-02-08 15:10:13 +0100

    Removed faulty functionality from label_Click methods. Added functionality to all nine buttons.

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -23,6 +23,16 @@ namespace TicTacToe
 
 		private void label1_Click(object sender, EventArgs e)
 		{
+			
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
 			if (currentPlayer == 1)
 			{
 				button1.Text = "X";
@@ -37,54 +47,132 @@ namespace TicTacToe
 			}
 		}
 
-		private void label2_Click(object sender, EventArgs e)
-		{
-
-		}
-
-		private void button1_Click(object sender, EventArgs e)
-		{
-
-		}
-
 		private void button2_Click(object sender, EventArgs e)
 		{
-
+			if (currentPlayer == 1)
+			{
+				button2.Text = "X";
+				button2.Enabled = false;
+				currentPlayer++;
+			}
+			else if (currentPlayer == 2)
+			{
+				button2.Text = "O";
+				button2.Enabled = false;
+				currentPlayer--;
+			}
 		}
 
 		private void button3_Click(object sender, EventArgs e)
 		{
-
+			if (currentPlayer == 1)
+			{
+				button3.Text = "X";
+				button3.Enabled = false;
+				currentPlayer++;
+			}
+			else if (currentPlayer == 2)
+			{
+				button3.Text = "O";
+				button3.Enabled = false;
+				currentPlayer--;
+			}
 		}
 
 		private void button4_Click(object sender, EventArgs e)
 		{
-
+			if (currentPlayer == 1)
+			{
+				button4.Text = "X";
+				button4.Enabled = false;
+				currentPlayer++;
+			}
+			else if (currentPlayer == 2)
+			{
+				button4.Text = "O";
+				button4.Enabled = false;
+				currentPlayer--;
+			}
 		}
 
 		private void button5_Click(object sender, EventArgs e)
 		{
-
+			if (currentPlayer == 1)
+			{
+				button5.Text = "X";
+				button5.Enabled = false;
+				currentPlayer++;
+			}
+			else if (currentPlayer == 2)
+			{
+				button5.Text = "O";
+				button5.Enabled = false;
+				currentPlayer--;
+			}
 		}
 
 		private void button6_Click(object sender, EventArgs e)
 		{
-
+			if (currentPlayer == 1)
+			{
+				button6.Text = "X";
+				button6.Enabled = false;
+				currentPlayer++;
+			}
+			else if (currentPlayer == 2)
+			{
+				button6.Text = "O";
+				button6.Enabled = false;
+				currentPlayer--;
+			}
 		}
 
 		private void button7_Click(object sender, EventArgs e)
 		{
-
+			if (currentPlayer == 1)
+			{
+				button7.Text = "X";
+				button7.Enabled = false;
+				currentPlayer++;
+			}
+			else if (currentPlayer == 2)
+			{
+				button7.Text = "O";
+				button7.Enabled = false;
+				currentPlayer--;
+			}
 		}
 
 		private void button8_Click(object sender, EventArgs e)
 		{
-
+			if (currentPlayer == 1)
+			{
+				button8.Text = "X";
+				button8.Enabled = false;
+				currentPlayer++;
+			}
+			else if (currentPlayer == 2)
+			{
+				button8.Text = "O";
+				button8.Enabled = false;
+				currentPlayer--;
+			}
 		}
 
 		private void button9_Click(object sender, EventArgs e)
 		{
-
+			if (currentPlayer == 1)
+			{
+				button9.Text = "X";
+				button9.Enabled = false;
+				currentPlayer++;
+			}
+			else if (currentPlayer == 2)
+			{
+				button9.Text = "O";
+				button9.Enabled = false;
+				currentPlayer--;
+			}
 		}
 	}
 }
